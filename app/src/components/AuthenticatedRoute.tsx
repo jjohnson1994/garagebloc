53b683a38b9873d0ce292fee0ee110aaa6e2b18f
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2022-02-23 07:43:49 +0000

    bugfix: no longer redirects to /profile on refresh

--- app/src/components/AuthenticatedRoute.tsx
+++ app/src/components/AuthenticatedRoute.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Redirect, useLocation } from "react-router-dom";
 import { useAppContext } from "../context/appContext";
+import LoadingSpinner from "./LoadingSpinner";
 
 interface Props {
   exact: boolean
@@ -9,13 +10,17 @@ interface Props {
 
 const AuthenticatedRoute: React.FC<Props> = ({ children, ...rest }) => {
   const { pathname, search } = useLocation();
-  const { isAuthenticated } = useAppContext();
+  const { isAuthenticated, isAuthenticating } = useAppContext();
 
   return (
     <Route {...rest}>
-      {isAuthenticated ? (
+      {isAuthenticating && (
+        <LoadingSpinner />
+      )}
+      {isAuthenticated && !isAuthenticating && (
         children
-      ) : (
+      )}
+      {!isAuthenticated && !isAuthenticating && (
         <Redirect to={`/login?redirect=${pathname}${search}`} />
       )}
     </Route>
