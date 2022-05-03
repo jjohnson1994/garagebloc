b9f4c9a84030217c3d0dfee50707164cd95d21ee
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2022-05-03 09:48:55 +0100

    landing page updates

--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -1,5 +1,7 @@
+import {Link} from "react-router-dom";
 import { appTitle } from "../config.json";
 import { useAppContext } from "../context/appContext";
+import Button, {Color} from "../elements/Button";
 import Home from "./Home";
 
 const Landing = () => {
@@ -9,13 +11,28 @@ const Landing = () => {
     <>
       <section className="section">
         <div className="container">
-          <h1 className="title">{ appTitle }</h1>
-          <p className="subtitle">The app for logging board climbs</p>
             <div className="columns">
               <div className="column">
                 <img src="exampleboard.png" alt="example board"/>
               </div>
-              <div className="column"></div>
+              <div className="column">
+                <div className="content">
+                  <h1 className="title">{ appTitle }</h1>
+                  <p className="subtitle">The app for logging board climbs</p>
+                  <p>Upload your board to the GarageBloc app, create and log climbs, track progress and share climbs:</p>
+                  <ul>
+                    <li>GarageBloc is free for everyone</li>
+                    <li>There are no limits on the number of holds or climbs</li>
+                    <li>Easily add new holds to a board, without having to re-log every climb</li>
+                    <li>Problems can be mirrored automatically</li>
+                  </ul>
+                  <div className="pt-4" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Link to="/signup">
+                      <Button color={ Color.isPrimary }>Get Started</Button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
       </section>
